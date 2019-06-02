23e1679c807479e8fcad2952a3bcc80b864bbef6
MHZDeveloper <mohamed.hichem.zayani@gmail.com>
2019-06-02 14:21:39 +0100

    Fifth Commit (String Calculator)

--- StringCalculator/src/main/java/StringCalculator.kt
+++ StringCalculator/src/main/java/StringCalculator.kt
@@ -12,22 +12,32 @@ fun add(number: String): String {
                 ||
                 number.length > 3
                 ||
-                !number.contains(".")
-                ||
                 number.getOccurances('.') > 1 -> return "0"
+
+        !number.contains(".") -> return number
 
         number[0].isDigit() && number[2].isDigit() -> {
             val num1 = number[0].toInt() - '0'.toInt()
             val num2 = number[2].toInt() - '0'.toInt()
             val result = num1 + num2
-            return number + " -> " + result.toString()
+            return result.toString()
         }
     }
-    return "null"
+    return number.replace(".", "")
 }
 
-fun addMultipleArgs(vararg numbers: String) {
-    numbers.forEach { number ->
-        println(add(number))
+fun addMultipleArgs(number: String): String {
+    if (number.isEmpty()) {
+        return "0"
     }
+    if (!number.contains(".")) {
+        return number
+    }
+    var result = 0
+    for (i in 1 until number.length) {
+        if (number[i] == number[i - 1] && number[i] == '.') return "error"
+        if (number[i].isDigit())
+            result = result + number[i].toInt() - '0'.toInt()
+    }
+    return result.toString()
 }
--- StringCalculator/src/test/java/StringCalculatorTest.kt
+++ StringCalculator/src/test/java/StringCalculatorTest.kt
@@ -11,8 +11,19 @@ class StringCalculatorTest{
     }
 
     @Test
+    fun should_return_number_if_no_dots(){
+        Assert.assertEquals("123",add("123"))
+    }
+
+    @Test
+    fun should_return_number_if_dots_are_in_the_beginning_or_the_end(){
+        Assert.assertEquals("23",add(".23"))
+        Assert.assertEquals("23",add("23."))
+    }
+
+    @Test
     fun should_return_zero_if_length_more_than_3(){
-        Assert.assertEquals("0",add("0785"))
+        Assert.assertEquals("0",add("7785"))
     }
 
     @Test
@@ -22,13 +33,17 @@ class StringCalculatorTest{
     }
 
     @Test
-    fun should_return_th_sum(){
+    fun should_return_the_sum(){
         Assert.assertEquals("8",add("0.8"))
         Assert.assertEquals("10",add("1.9"))
     }
 
     @Test
-    fun should_return_right_answer_for_multiple_args(){
-        addMultipleArgs("0.8","1.9")
+    fun should_return_right_sum_for_multiple_arguments(){
+        Assert.assertEquals("10",addMultipleArgs("0.8.2"))
+        Assert.assertEquals("11",addMultipleArgs(".8.3"))
+        Assert.assertEquals("8",addMultipleArgs("0.8."))
+        Assert.assertEquals("error",addMultipleArgs("0..2"))
     }
+
 }
