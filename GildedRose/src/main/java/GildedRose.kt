31a13bbbfa3176705c828322dd0548164dd48062
MHZDeveloper <mohamed.hichem.zayani@gmail.com>
2020-08-06 01:38:10 +0100

    refactoring tests

--- GildedRose/src/main/java/GildedRose.kt
+++ GildedRose/src/main/java/GildedRose.kt
@@ -4,33 +4,32 @@ class GildedRose(var items: Array<Item>) {
         for (i in items.indices) {
             if (!items[i].name.contains("Sulfuras")) {
                 items[i].sellIn--
-                if (items[i].name.contains("Aged Brie")) {
-                    if (items[i].quality in 0..49)
-                        items[i].quality++
-                } else if (items[i].name.contains("Backstage")) {
-                    if (items[i].sellIn >= 0) {
-                        if (items[i].sellIn in 0..5)
-                            items[i].quality += 3
-                        else if (items[i].sellIn in 0..10)
-                            items[i].quality += 2
-                        else
-                            items[i].quality++
-                    } else {
-                        items[i].quality = 0
+                items[i].quality--
+                if (items[i].sellIn >= 0) {
+                    if (items[i].name.contains("Aged Brie")) {
+                        items[i].quality += 2
+                    } else if (items[i].name.contains("Backstage")) {
+                        when(items[i].sellIn){
+                            in 0..5 -> items[i].quality += 4
+                            in 0..10 -> items[i].quality += 3
+                            else -> items[i].quality += 2
+                        }
+                    } else if (items[i].name.contains("Conjured")) {
+                        items[i].quality--
                     }
                 } else {
-                    var degradation = 1
+                    items[i].quality--
                     if (items[i].name.contains("Conjured"))
-                        degradation++
-                    if (items[i].quality > 0) {
-                        if (items[i].sellIn > 0)
-                            items[i].quality-= degradation
-                        else
-                            items[i].quality -= 2*degradation
-                    }
+                        items[i].quality -= 2
+                    else if (items[i].name.contains("Backstage"))
+                        items[i].quality = 0
                 }
+                if (items[i].quality < 0)
+                    items[i].quality = 0;
+                else if (items[i].quality > 50)
+                    items[i].quality = 50
             }
         }
     }
-
 }
+
--- GildedRose/src/test/java/GildedRoseTest.kt
+++ GildedRose/src/test/java/GildedRoseTest.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 class GildedRoseTest {
 
     @Test
-    fun update_item() {
+    fun should_decrement_sellin_and_quality_for_any_item() {
         //given
         val items = arrayOf(Item("+5 Dexterity Vest", 10, 20))
         val app = GildedRose(items)
@@ -16,135 +16,144 @@ class GildedRoseTest {
     }
 
     @Test
-    fun update_item_with_zero_Quality() {
+    fun should_not_decrement_quality_when_equal_to_zero_for_any_item() {
         //given
-        val items = arrayOf(Item("+5 Dexterity Vest", -1, 0))
+        val items = arrayOf(Item("Aged Brie", -5, 0),
+                Item("+5 Dexterity Vest", -5, 0),
+                Item("Sulfuras, Hand of Ragnaros", -5, 0),
+                Item("Backstage passes to a TAFKAL80ETC concert", -20, 0),
+                Item("Conjured", -5, 0))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(-2, app.items[0].sellIn)
         assertEquals(0, app.items[0].quality)
+        assertEquals(0, app.items[1].quality)
+        assertEquals(0, app.items[2].quality)
+        assertEquals(0, app.items[3].quality)
+        assertEquals(0, app.items[4].quality)
     }
 
     @Test
-    fun update_item_with_zero_SellIn() {
+    fun should_not_increment_quality_when_equal_to_50_for_any_item() {
         //given
-        val items = arrayOf(Item("+5 Dexterity Vest", 0, 10))
+        val items = arrayOf(Item("Aged Brie", 5, 50),
+                Item("Sulfuras, Hand of Ragnaros", 5, 50),
+                Item("Backstage passes to a TAFKAL80ETC concert", 20, 50))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(-1, app.items[0].sellIn)
-        assertEquals(8, app.items[0].quality)
+        assertEquals(50, app.items[0].quality)
+        assertEquals(50, app.items[1].quality)
+        assertEquals(50, app.items[2].quality)
     }
 
     @Test
-    fun update_AgedBrie_item() {
+    fun should_decrement_twice_quality_when_sellin_negative() {
+        //given
+        val items = arrayOf(Item("Aged Brie", 0, 10),
+                Item("+5 Dexterity Vest", 0, 10),
+                Item("Sulfuras, Hand of Ragnaros", 0, 10),
+                Item("Backstage passes to a TAFKAL80ETC concert", 0, 10),
+                Item("Conjured", 0, 10))
+        val app = GildedRose(items)
+        //when
+        app.updateQuality()
+        //then
+        assertEquals(8, app.items[0].quality)
+        assertEquals(8, app.items[1].quality)
+        assertEquals(10, app.items[2].quality)
+        assertEquals(0, app.items[3].quality)
+        assertEquals(6, app.items[4].quality)
+    }
+
+    @Test
+    fun should_increment_quality_for_aged_brie_item_when_sellin_positive() {
         //given
         val items = arrayOf(Item("Aged Brie", 5, 10))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(4, app.items[0].sellIn)
         assertEquals(11, app.items[0].quality)
     }
 
     @Test
-    fun update_AgedBrie_item_with_50_Quality() {
-        //given
-        val items = arrayOf(Item("Aged Brie", 5, 50))
-        val app = GildedRose(items)
-        //when
-        app.updateQuality()
-        //then
-        assertEquals(4, app.items[0].sellIn)
-        assertEquals(50, app.items[0].quality)
-    }
-
-    @Test
-    fun update_Sulfuras_item() {
+    fun should_not_update_sulfuras_item() {
         //given
         val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", 5, 40))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(5, app.items[0].sellIn)
         assertEquals(40, app.items[0].quality)
     }
 
     @Test
-    fun update_Backstage_item() {
+    fun should_increment_quality_by_1_for_backstage_item_when_sellin_more_than_10_days() {
         //given
         val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 20, 40))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(19, app.items[0].sellIn)
         assertEquals(41, app.items[0].quality)
     }
 
     @Test
-    fun update_Backstage_item_with_SellIn_equal_or_less_than_10() {
+    fun should_increment_quality_by_2_for_backstage_item_when_sellin_less_than_10_days() {
         //given
         val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 40))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(9, app.items[0].sellIn)
         assertEquals(42, app.items[0].quality)
     }
 
     @Test
-    fun update_Backstage_item_with_SellIn_equal_or_less_than_5() {
+    fun should_increment_quality_by_3_for_backstage_item_when_sellin_less_than_5_days() {
         //given
         val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 40))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(4, app.items[0].sellIn)
         assertEquals(43, app.items[0].quality)
     }
 
     @Test
-    fun update_Backstage_item_with_SellIn_less_than_0() {
+    fun should_change_quality_to_0_for_backstage_item_when_sellin_negative() {
         //given
         val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 40))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(-1, app.items[0].sellIn)
         assertEquals(0, app.items[0].quality)
     }
 
     @Test
-    fun update_Conjured_item_with_SellIn_less_than_0() {
+    fun should_decrement_quality_by_4_for_conjured_item_when_sellin_negative() {
         //given
         val items = arrayOf(Item("Conjured", 0, 40))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(-1, app.items[0].sellIn)
         assertEquals(36, app.items[0].quality)
     }
 
 
     @Test
-    fun update_Conjured_item_with_SellIn_more_than_0() {
+    fun should_decrement_quality_by_2_for_conjured_item_when_sellin_positive() {
         //given
         val items = arrayOf(Item("Conjured", 5, 40))
         val app = GildedRose(items)
         //when
         app.updateQuality()
         //then
-        assertEquals(4, app.items[0].sellIn)
         assertEquals(38, app.items[0].quality)
     }
 
