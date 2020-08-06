b3f8f9d8c48fc2dce88607d207fabc3503e6f64d
MHZDeveloper <mohamed.hichem.zayani@gmail.com>
2020-08-06 12:18:01 +0100

    refactoring gilded rose method

--- GildedRose/src/main/java/GildedRose.kt
+++ GildedRose/src/main/java/GildedRose.kt
@@ -6,28 +6,28 @@ class GildedRose(var items: Array<Item>) {
                 items[i].sellIn--
                 items[i].quality--
                 if (items[i].sellIn >= 0) {
-                    if (items[i].name.contains("Aged Brie")) {
-                        items[i].quality += 2
-                    } else if (items[i].name.contains("Backstage")) {
-                        when(items[i].sellIn){
-                            in 0..5 -> items[i].quality += 4
-                            in 0..10 -> items[i].quality += 3
-                            else -> items[i].quality += 2
+                    when {
+                        items[i].name.contains("Aged Brie") -> items[i].quality += 2
+                        items[i].name.contains("Backstage") -> {
+                            when (items[i].sellIn) {
+                                in 0..5 -> items[i].quality += 4
+                                in 0..10 -> items[i].quality += 3
+                                else -> items[i].quality += 2
+                            }
                         }
-                    } else if (items[i].name.contains("Conjured")) {
-                        items[i].quality--
+                        items[i].name.contains("Conjured") -> items[i].quality--
                     }
                 } else {
                     items[i].quality--
-                    if (items[i].name.contains("Conjured"))
-                        items[i].quality -= 2
-                    else if (items[i].name.contains("Backstage"))
-                        items[i].quality = 0
+                    when {
+                        items[i].name.contains("Conjured") -> items[i].quality -= 2
+                        items[i].name.contains("Backstage") -> items[i].quality = 0
+                    }
                 }
-                if (items[i].quality < 0)
-                    items[i].quality = 0;
-                else if (items[i].quality > 50)
-                    items[i].quality = 50
+                when {
+                    items[i].quality < 0 -> items[i].quality = 0
+                    items[i].quality > 50 -> items[i].quality = 50
+                }
             }
         }
     }
